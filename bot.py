b11a3d0c648fa0fe7c53e371bf29394cbdc7d019
FerreiraDev25 <emaildeestudardealex@gmail.com>
2025-03-21 09:32:39 -0300

    Segundo commit ajustando comandos

--- bot.py
+++ bot.py
@@ -5,13 +5,12 @@ from telegram.ext import Application, CommandHandler, MessageHandler, filters, _
 TOKEN = '7756070091:AAG8TcrPW602VVPlAnHU_O5nuwn8OVZSk-c'
 
 # Função que responde ao comando /start
-async def start(update: Update, context: CallbackContext):
-    await update.message.reply_text('Olá! Eu sou o seu bot do Telegram! \nDigite alguma coisa para testar.')
+#Espaço para escrever
 
 #Função que responde mensagens comuns
 async def responder(update: Update, context: CallbackContext):
     mensagem = update.message.text
-    await update.message.reply_text(f'Você disse: {mensagem}')
+    await update.message.reply_text(f'Olá! Eu sou o seu bot do Telegram! \nEstes são os comandos disponíveis no momento. \n - /start \n - /ajuda \n - /sobre')
 
 # Criando o aplicativo bot
 app = Application.builder().token(TOKEN).build()
@@ -26,7 +25,7 @@ app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, responder))
 async def ajuda(update: Update, context: CallbackContext):
     await update.message.reply_text('Aqui estão os comandos disponíveis:\n/start - Inicia o bot\n/ajuda - mostra este menu\n/sobre - sobre o bot')
 
-# Função para exibir informações osbre o bot
+# Função para exibir informações sobre o bot
 async def sobre(update: Update, context: CallbackContext):
     await update.message.reply_text('Sou um bot criado em Python! \nMe ajude a crescer dando sugestões!')
 
