0f3578bdc216eda3f350d47de234d72d7e24a9c8
FerreiraDev25 <157470533+FerreiraDev25@users.noreply.github.com>
2025-03-25 10:15:26 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from threading import Thread
 import asyncio
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext, ContextTypes
+import subprocess
 
 # Seu TOKEN doBotFather aqui!
 TOKEN = '7756070091:AAG8TcrPW602VVPlAnHU_O5nuwn8OVZSk-c'
@@ -14,11 +15,22 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 #Função que responde mensagens comuns
 async def responder(update: Update, context: CallbackContext):
     mensagem = update.message.text
-    await update.message.reply_text(f'Olá! Eu sou o seu bot do Telegram! \nEstes são os comandos disponíveis no momento. \n - /start \n - /ajuda \n - /sobre')
+    await update.message.reply_text(
+        'Olá! Eu sou o seu bot do Telegram! \n'
+        'Estes são os comandos disponíveis no momento. \n' 
+        '- /start \n'
+        ' - /ajuda \n'
+        ' - /sobre'
+    )
 
 # Função para exibir ajuda
 async def ajuda(update: Update, context: CallbackContext):
-    await update.message.reply_text('Aqui estão os comandos disponíveis:\n/start - Inicia o bot\n/ajuda - mostra este menu\n/sobre - sobre o bot')
+    await update.message.reply_text(
+        'Aqui estão os comandos disponíveis:\n'
+        '/start - Inicia o bot\n'
+        '/ajuda - mostra este menu\n'
+        '/sobre - sobre o bot'
+    )
 
 # Função para exibir informações sobre o bot
 async def sobre(update: Update, context: CallbackContext):
@@ -43,17 +55,24 @@ def home():
     return "Bot está rodando!"
 
 #Função para iniciar o bot no asyncio
-async def start_bot():
-    await app.run_polling()
+def start_bot():
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(app.run_polling())
 
-#Função para iniciar o bot em task assíncrona
-def run_bot():
-    loop = asyncio.get_event_loop()
-    loop.create_task(start_bot()) # inicia o bot como uma tarefa assíncrona
-    loop.run_forever() # Garante que o loop de evento continue rodando
+#Função para manter o Flask ativo, realizando o "ping" periodicamente
+def keep_flask_alive():
+    while True:
+        try:
+            #Rodando o comando curl para manter o Flask ativo
+            subprocess.run(["curl", "-s", "https://seu-bot-no-render.com/"], stdout=subprocess.DEVNULL)
+        except subprocess.CalledProcessError as e:
+            print(f'Erro ao fazer o ping: {e}')
+        #Aguarda 10 minutos (600 segundos) antes de fazer o próximo ping
+        time.sleep(600)
 
 #Inicia o bot e o servidor Flask em threads separadas
-Thread(target=start_bot).start()
-
 if __name__ == "__main__":
+    Thread(target=start_bot, daemon=True).start()
+    Thread(target=keep_flask-alive, daemon=True).strat()
     flask_app.run(host="0.0.0.0", port=8080)
